793eabb63f8a19b9f4c6f0624e841937471f37c1
FrixxerNynd <148486300+FrixxerNynd@users.noreply.github.com>
2024-06-07 14:51:46 +0000

    si

--- Parcial_1/10_sets/sets.py
+++ Parcial_1/10_sets/sets.py
@@ -1,7 +1,6 @@
 """
- Sets.- 
+ Sets
   Es un tipo de datos para tener una coleccion de valores pero no tiene ni indice ni orden
-
   Set es una colección desordenada, inmutable* y no indexada. No hay miembros duplicados.
 """
 
--- Parcial_1/11_tuple/tuple.py
+++ Parcial_1/11_tuple/tuple.py
@@ -16,8 +16,7 @@ print(tupla1)
 
 tupla2 = ("abc", 34, True, 40, "male")
 print(tupla2)
-
-
-# Metodos de tuplas
-# count()	Returns the number of times a specified value occurs in a tuple
-# index() Searches the tuple for a specified value and returns the position of where it was found
+"""
+Metodos de tuplas
+count()	Returns the number of times a specified value occurs in a tuple
+index() Searches the tuple for a specified value and returns the position of where it was found """
